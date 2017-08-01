e42c141b8adaf349fc0dc5bf036365730860afef
Misgana Bayetta <misgana.bayetta@gmail.com>
2017-08-01 15:10:30 +0800

    Rename extension

--- src/sensor_fusion/src/cpp/FusionServer.cpp
+++ src/sensor_fusion/src/cpp/FusionServer.cpp
@@ -1,34 +1,6 @@
 #include "ros/ros.h"
 #include "Fusion.h"
 
-class FusionServer{
-    private:
-        std::vector<FusionModule*> _modules;
-        ros::Rate _loop_rate;
-
-    public:
-        FusionServer(unsigned int loop_rate = 10);
-        ~FusionServer();
-
-        // Dynamical loader for ROS fusion nodes
-        bool load(std::string path);
-        void unload(std::string fusion);
-
-
-        // This is where all the ros topics are published.
-        void run(void){
-            while (ros::ok()){
-                for(const FusionModule* module : _modules){
-                    module->publish();
-                } 
-                ros::spinOnce();
-                loop_rate.sleep();
-            }
-        }
-        // Load Fusion libraries
-        // Unload Fusers
-};
-
 FusionServer::FusionServer(unsigned int loop_rate/*=10*/) :_loop_rate(loop_rate){
 
 }
